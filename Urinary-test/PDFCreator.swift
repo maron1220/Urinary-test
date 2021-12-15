ea41bd3f836ad1f8280982845d3b60302d9bd641
細川聖矢 <hosokawakiyoshiya@MacBook-Pro.local>
2021-12-15 19:23:49 +0900

    create extension PDFCreator

--- Urinary-test/PDFCreator.swift
+++ Urinary-test/PDFCreator.swift
@@ -27,3 +27,38 @@ class PDFCreator: NSObject {
     }
 
 }
+
+extension PDFCreator{
+    private func addTitle(title : String){
+        let textRect = CGRect(x:20,y:20,width: pageReact.width - 40 ,height: 40)
+        title.draw(in:textRect,withAttributes:[NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 30)])
+    }//assTitle
+    
+    private func addBody(body:String){
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .justified
+        
+        let attributes = [
+            NSAttributedString.Key.font:UIFont.systemFont(ofSize: 20),
+            NSAttributedString.Key.paragraphStyle:paragraphStyle,
+            NSAttributedString.Key.foregroundColor:UIColor.gray
+        ]//attributes
+        
+        let bodyRect = CGRect(x: 20, y: 70, width: pageReact.width - 40, height: pageReact.height - 80)
+        body.draw(in:bodyRect,withAttributes:attributes)
+    }//addBody
+}//extension PDFCreator
+
+extension PDFCreator{
+    func pdfData(title:String,body:String) -> Data?{
+        if let renderer = self.renderer{
+            let data = renderer.pdfData{ ctx in
+                ctx.beginPage()
+                addTitle(title: title)
+                addBody(body: body)
+            }
+            return data
+        }//if let renderer
+        return nil
+    }//func pdfData
+}//extension PDFCreator
