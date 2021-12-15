c3f4eecbcaa2a0199c3709cd460a1bb996b1ea1f
細川聖矢 <hosokawakiyoshiya@MacBook-Pro.local>
2021-12-15 22:18:16 +0900

    add ShareView

--- Urinary-test/ShareView.swift
+++ Urinary-test/ShareView.swift
@@ -7,9 +7,23 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 //シェアしたいPDFファイルを表示する
 
-let sharer = UIActivityViewController(activityItems: ["Hello world"], applicationActivities: nil)
+//let sharer = UIActivityViewController(activityItems: ["Hello world"], applicationActivities: nil)
 //text,images,strings,UPLsなどをシェアするのにUIActivityViewControllerが必要。
 //activityItemsはArrayで返す。
-present(sharer,animated:true)
+//present(sharer,animated:true)
+
+struct ShareView:UIViewControllerRepresentable{
+    let activityItems:[Any]
+    let applicationActivities:[UIActivity]? = nil
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ShareView>) -> UIActivityViewController{
+        return UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+    }//func UIViewController
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ShareView>) {
+        //empty
+    }//func updateUIViewController
+}//UIViewControllerRepresentable
